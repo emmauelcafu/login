1015e148b9053f5545667bdc452b8cb08297c153
emmanuelcafu <emmauelcafu@gmail.com>
2023-10-03 12:43:21 -0500

    primer actualizacion

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import Logins from './views/login/login.component';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Logins/>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/views/login/login.component.jsx
@@ -0,0 +1,7 @@
+function Logins(params) {
+    return(
+        <dir> este es Logins</dir>
+    )
+}
+
+export default Logins;
